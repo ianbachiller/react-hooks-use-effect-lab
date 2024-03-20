1a8f91db66daf7621bbaa2a4a904954929d42b3b
Ian Bachiller <ianbachise@gmail.com>
2024-03-20 01:47:50 -0400

    compeltes react hooks use -effect lab

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,11 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
+  let timer
 
   // add useEffect code
+  useEffect(() => {
+    timer = setInterval(() => {
+      setTimeRemaining((prevTime) => {
+        if(prevTime === 0){
+          clearInterval(timer)
+          onAnswered(false)
+          return 10
+        } else {
+          return prevTime - 1
+        }
+      })
+    }, 1000)
+    return () => clearTimeout(timer) 
+  }, [question, onAnswered])
+
+
 
   function handleAnswer(isCorrect) {
+    clearInterval(timer)
     setTimeRemaining(10);
     onAnswered(isCorrect);
   }
